c8e66be36561ec8c256044ef779632f9fb990701
Krzysztof Tatara <krzysiektt@gmail.com>
2022-02-27 19:31:50 +0100

    basic test

--- src/test/java/org/future/epam/employees/homeTask/DeliverToPage.java
+++ src/test/java/org/future/epam/employees/homeTask/DeliverToPage.java
@@ -1,26 +1,48 @@
 package org.future.epam.employees.homeTask;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class DeliverToPage {
     private WebDriver driver;
+    private  WebDriverWait wait;
 
     @FindBy(id ="nav-global-location-popover-link")
-    private  WebElement findBy;
+    private  WebElement deliverTo;
+
+
+    @FindBy(id = "GLUXZipUpdateInput")
+    private WebElement searchPostCode;
+
+    @FindBy (how = How.XPATH, using ="//*[@aria-labelledby ='GLUXZipUpdate-announce']")
+    private WebElement applyButton;
 
     public DeliverToPage(WebDriver driver){
         PageFactory.initElements(driver,this);
         this.driver=driver;
+        wait = new WebDriverWait(driver,10);
     }
 
+
+
     public DeliverToPage openPopUp(){
-        findBy.click();
+        deliverTo.click();
     return new DeliverToPage(driver);
+    }
+
+    public DeliverToPage fillPostCode(String postCode){
+        wait.until(ExpectedConditions.presenceOfElementLocated((By.id("GLUXZipUpdateInput"))));
+        searchPostCode.sendKeys(postCode);
+        applyButton.click();
+        return new DeliverToPage(driver);
     }
 
 }
--- src/test/java/org/future/epam/employees/homeTask/DeliverToTest.java
+++ src/test/java/org/future/epam/employees/homeTask/DeliverToTest.java
@@ -1,16 +1,21 @@
 package org.future.epam.employees.homeTask;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 @Test
 public class DeliverToTest {
     public void test() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "src/test/java/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.com/");
         DeliverToPage deliverToPage = new DeliverToPage(driver).openPopUp();
-
+        Assert.assertTrue(driver.findElement(By.id("a-popover-header-1")).getSize() != null);
+        DeliverToPage checkPostcode = deliverToPage.fillPostCode("85001");
+        Assert.assertTrue(driver!=null);
     }
 }
