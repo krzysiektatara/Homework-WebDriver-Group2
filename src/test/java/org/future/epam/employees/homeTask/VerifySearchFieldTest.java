0ceead0f0634743f0818a5add1031e33fb5d007b
Irina Vodopian <irinavoddan@bk.ru>
2022-02-27 20:59:27 +0100

    SearchFieldTest with DataProvider

--- src/test/java/org/future/epam/employees/homeTask/VerifySearchFieldTest.java
+++ src/test/java/org/future/epam/employees/homeTask/VerifySearchFieldTest.java
@@ -1,5 +1,4 @@
 package org.future.epam.employees.homeTask;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,38 +7,57 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 
-public class VerifySearchFieldTest {
-        @Test
-        public void SearchFieldTest(){
 
-            WebDriverManager.chromedriver().browserVersion("97").setup();
-            WebDriver driver = new ChromeDriver();
+public class VerifySearchFieldTest {
+    WebDriver driver = setUp();
+
+    @DataProvider(name = "requests")
+    public Object[][] requests() {
+        return new Object[][]
+                {
+                        {"toy"},
+                        {"phone"},
+                        {"laptop"},
+                        {"xbxb"},
+                        {"!!!"}
+                };
+    }
+
+    @BeforeTest
+    public WebDriver setUp()
+    {
+        WebDriverManager.chromedriver().browserVersion("97").setup();
+        WebDriver driver = new ChromeDriver();
+        return driver;
+    }
+
+        @Test(dataProvider = "requests")
+        public void SearchFieldTest(String request){
             driver.get("https://www.amazon.com/");
 
-            Assert.assertEquals(driver.getCurrentUrl(), "https://www.amazon.com/",
-                    "The current link is not as expected");
-
             WebElement searchField = driver.findElement(By.id("twotabsearchtextbox"));
-            searchField.sendKeys("books");
+            searchField.sendKeys(request);
 
             WebElement searchButton = driver.findElement(By.id("nav-search-submit-button"));
             searchButton.click();
 
-            Assert.assertEquals(driver.getCurrentUrl(), "https://www.amazon.com/b?node=283155",
-                    "The current link is not as expected");
-
             WebElement searchResult = new WebDriverWait(driver, Duration.ofSeconds(10))
-                    .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"a-page\"]/div[2]/div[2]/div[2]/div[1]/div/div[1]/h3[1]")));
+                    .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"search\"]/span/div/h1/div/div[1]/div/div/span[3]")));
 
-            System.out.println(searchResult.getText());
-            Assert.assertTrue(searchResult.getText().contains("Books"));
+            Assert.assertTrue(searchResult.getText().contains(request),"The current link is not as expected");
 
+        }
+        @AfterTest
+    public void Close()
+        {
             driver.close();
             driver.quit();
         }
+    }
 
-}
