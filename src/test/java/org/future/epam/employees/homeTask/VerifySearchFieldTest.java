7d6f3af2644ddc2fb57aa4ac64d3cde5d440ed90
Irina Vodopian <irinavoddan@bk.ru>
2022-02-28 19:37:53 +0100

    SearchFieldTest with DataProvider and 3 tests

--- src/test/java/org/future/epam/employees/homeTask/VerifySearchFieldTest.java
+++ src/test/java/org/future/epam/employees/homeTask/VerifySearchFieldTest.java
@@ -15,43 +15,85 @@ import java.time.Duration;
 
 
 public class VerifySearchFieldTest {
-    WebDriver driver = setUp();
 
-    @DataProvider(name = "requests")
+    @DataProvider(name = "IncorrectRequests")
+    public Object[][] incorrectRequests() {
+        return new Object[][]
+                {
+                        {"lkjlkjsnlgksd'dfkg;klnl34058734-591=49v8-0 7", "//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div[1]/div/div/div/span[1]"},
+                        {"><><>","//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div[1]/div/div/div/div[1]/span[1]"}
+                };
+    }
+
+    @DataProvider(name = "CorrectRequests")
     public Object[][] requests() {
         return new Object[][]
                 {
                         {"toy"},
                         {"phone"},
                         {"laptop"},
-                        {"xbxb"},
-                        {"!!!"}
                 };
     }
+
+    @DataProvider(name = "ContainWords")
+    public Object[][] words() {
+        return new Object[][]
+                {
+                        {"Keyboard","//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div[2]/div/div/div/div/div/div/div/div[2]/div/div/div[1]/h2/a/span"},
+                        {"Hello Kitty", "//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div[2]/div/div/div/div/div/div/div[2]/div[2]/h2/a/span"},
+                };
+    }
+
+    WebDriver driver = setUp();
 
     @BeforeTest
     public WebDriver setUp()
     {
         WebDriverManager.chromedriver().browserVersion("97").setup();
         WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com/");
         return driver;
     }
 
-        @Test(dataProvider = "requests")
-        public void SearchFieldTest(String request){
-            driver.get("https://www.amazon.com/");
+    public void sendRequest(String request)
+    {
+        WebElement searchField = driver.findElement(By.id("twotabsearchtextbox"));
+        searchField.clear();
+        searchField.sendKeys(request);
 
-            WebElement searchField = driver.findElement(By.id("twotabsearchtextbox"));
-            searchField.sendKeys(request);
+        WebElement searchButton = driver.findElement(By.id("nav-search-submit-button"));
+        searchButton.click();
+    }
 
-            WebElement searchButton = driver.findElement(By.id("nav-search-submit-button"));
-            searchButton.click();
+    public WebElement searchForResult(String path)
+    {
+        return new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(path)));
+    }
 
-            WebElement searchResult = new WebDriverWait(driver, Duration.ofSeconds(10))
-                    .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"search\"]/span/div/h1/div/div[1]/div/div/span[3]")));
 
+        @Test(dataProvider = "IncorrectRequests")
+        public void incorrectRequestTest(String incorrectR, String path){
+            sendRequest(incorrectR);
+            WebElement searchResult = searchForResult(path);
+            Assert.assertTrue(searchResult.getText().contains("No results for"),"The expected message did not appear");
+        }
+
+
+        @Test(dataProvider = "CorrectRequests")
+        public void correctRequestTest(String request){
+            sendRequest(request);
+            WebElement searchResult = searchForResult("//*[@id=\"search\"]/span/div/h1/div/div[1]/div/div/span[3]");
             Assert.assertTrue(searchResult.getText().contains(request),"The current link is not as expected");
+        }
 
+
+        @Test(dataProvider = "ContainWords")
+        public void containsWordTest(String request, String path)
+        {
+            sendRequest(request);
+            WebElement searchResult = searchForResult(path);
+            Assert.assertTrue(searchResult.getText().contains(request),"The current page does not contain expected result");
         }
 
         @AfterTest
