b9852cd973ef6ca1b66d461b12614bbf95cd296a
Krzysztof Tatara <krzysiektt@gmail.com>
2022-03-01 14:24:18 +0100

    working test v_1

--- src/test/java/org/future/epam/employees/homeTask/DeliverToTest.java
+++ src/test/java/org/future/epam/employees/homeTask/DeliverToTest.java
@@ -1,5 +1,6 @@
 package org.future.epam.employees.homeTask;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,20 +12,49 @@ import org.testng.annotations.Test;
 
 
 public class DeliverToTest {
-@DataProvider(name = "postCodes")
-public Object[][] postCodes(){
-    return new Object[][]{ {"36104"},{"99801"},{"85001"},{"72201"},{"95814"},{"80202"},
-};
-}
+    @DataProvider(name = "postCodes")
+    public Object[][] postCodes() {
+        return new Object[][]{{"36104"}, {"99801"},
+        };
+    }
 
     @Test(dataProvider = "postCodes")
-    public void test(String postCode) {
+    public void postCodesTest(String postCode) {
         System.setProperty("webdriver.chrome.driver", "src/test/java/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.com/");
-        DeliverToPage deliverToPage = new DeliverToPage(driver).openPopUp().fillPostCode(postCode);
+        DeliverToPage deliverToPage = new DeliverToPage(driver).getPopUp().fillPostCode(postCode);
+        Assert.assertEquals(postCode, deliverToPage.returnPostCode().getAttribute("innerHTML"));
 
-        Assert.assertTrue(deliverToPage.returnPostCode().getAttribute("innerHTML").equals(postCode));
+    }
+
+    @Test
+    public void isPolandPresentTest() {
+        System.setProperty("webdriver.chrome.driver", "src/test/java/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com/");
+        driver.manage().window().maximize();
+        DeliverToPage deliverToPage = new DeliverToPage(driver).getPopUp().rollMenu();
+        Assert.assertTrue(deliverToPage.getPoland().isDisplayed());
+    }
+
+    @Test
+    public void countryInOrderTest() {
+        System.setProperty("webdriver.chrome.driver", "src/test/java/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com/");
+        driver.manage().window().maximize();
+        DeliverToPage deliverToPage = new DeliverToPage(driver).getPopUp().rollMenu();
+        deliverToPage.getPoland().click();
+        deliverToPage.getDoneButton().click();
+        driver.navigate().refresh();
+        deliverToPage.getHeadsetCategory().click();
+        deliverToPage.getItem().click();
+        Assert.assertTrue(deliverToPage.getShippingDescription().getText().contains("Poland"));
+
+
+
+
 
     }
 }
